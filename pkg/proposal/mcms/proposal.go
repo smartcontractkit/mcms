1118351b7e9c7d10dc31371214777da8f9545563
Akhil Chainani <akhil.chainani@smartcontract.com>
2024-10-03 16:30:49 -0700

    port golib from ccip-owner-contracts into here

---
+++ pkg/proposal/mcms/proposal.go
@@ -0,0 +1,127 @@
+package mcms
+
+import (
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/smartcontractkit/mcms/pkg/errors"
+)
+
+type ChainMetadata struct {
+	StartingOpCount uint64         `json:"startingOpCount"`
+	MCMAddress      common.Address `json:"mcmAddress"`
+}
+
+// MCMSProposal is a struct where the target contract is an MCMS contract
+// with no forwarder contracts. This type does not support any type of atomic contract
+// call batching, as the MCMS contract natively doesn't support batching
+type MCMSProposal struct {
+	Version              string      `json:"version"`
+	ValidUntil           uint32      `json:"validUntil"`
+	Signatures           []Signature `json:"signatures"`
+	OverridePreviousRoot bool        `json:"overridePreviousRoot"`
+
+	// Map of chain identifier to chain metadata
+	ChainMetadata map[ChainIdentifier]ChainMetadata `json:"chainMetadata"`
+
+	// This is intended to be displayed as-is to signers, to give them
+	// context for the change. File authors should templatize strings for
+	// this purpose in their pipelines.
+	Description string `json:"description"`
+
+	// Operations to be executed
+	Transactions []ChainOperation `json:"transactions"`
+}
+
+func NewProposal(
+	version string,
+	validUntil uint32,
+	signatures []Signature,
+	overridePreviousRoot bool,
+	chainMetadata map[ChainIdentifier]ChainMetadata,
+	description string,
+	transactions []ChainOperation,
+) (*MCMSProposal, error) {
+	proposal := MCMSProposal{
+		Version:              version,
+		ValidUntil:           validUntil,
+		Signatures:           signatures,
+		OverridePreviousRoot: overridePreviousRoot,
+		ChainMetadata:        chainMetadata,
+		Description:          description,
+		Transactions:         transactions,
+	}
+
+	err := proposal.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return &proposal, nil
+}
+
+func NewProposalFromFile(filePath string) (*MCMSProposal, error) {
+	var out MCMSProposal
+	err := FromFile(filePath, &out)
+	if err != nil {
+		return nil, err
+	}
+	return &out, nil
+}
+
+func (m *MCMSProposal) Validate() error {
+	if m.Version == "" {
+		return &errors.ErrInvalidVersion{
+			ReceivedVersion: m.Version,
+		}
+	}
+
+	// Get the current Unix timestamp as an int64
+	currentTime := time.Now().Unix()
+
+	if m.ValidUntil <= uint32(currentTime) {
+		// ValidUntil is a Unix timestamp, so it should be greater than the current time
+		return &errors.ErrInvalidValidUntil{
+			ReceivedValidUntil: m.ValidUntil,
+		}
+	}
+
+	if len(m.ChainMetadata) == 0 {
+		return &errors.ErrNoChainMetadata{}
+	}
+
+	if len(m.Transactions) == 0 {
+		return &errors.ErrNoTransactions{}
+	}
+
+	if m.Description == "" {
+		return &errors.ErrInvalidDescription{
+			ReceivedDescription: m.Description,
+		}
+	}
+
+	// Validate all chains in transactions have an entry in chain metadata
+	for _, t := range m.Transactions {
+		if _, ok := m.ChainMetadata[t.ChainIdentifier]; !ok {
+			return &errors.ErrMissingChainDetails{
+				ChainIdentifier: uint64(t.ChainIdentifier),
+				Parameter:       "chain metadata",
+			}
+		}
+	}
+	return nil
+}
+
+func (m *MCMSProposal) ToExecutor(sim bool) (*Executor, error) {
+	// Create a new executor
+	executor, err := NewProposalExecutor(m, sim)
+	if err != nil {
+		return nil, err
+	}
+
+	return executor, nil
+}
+
+func (m *MCMSProposal) AddSignature(signature Signature) {
+	m.Signatures = append(m.Signatures, signature)
+}
---
+++ pkg/proposal/types.go
@@ -0,0 +1,39 @@
+package proposal
+
+import (
+	"errors"
+
+	"github.com/smartcontractkit/mcms/pkg/proposal/mcms"
+	"github.com/smartcontractkit/mcms/pkg/proposal/timelock"
+)
+
+type ProposalType string
+
+const (
+	// MCMSProposalType is a proposal type for the MCMS contract.
+	MCMS ProposalType = "MCMS"
+	// MCMSWithTimelock is a proposal type for the MCMS contract with timelock.
+	MCMSWithTimelock ProposalType = "MCMSWithTimelock"
+)
+
+var StringToProposalType = map[string]ProposalType{
+	"MCMS":             MCMS,
+	"MCMSWithTimelock": MCMSWithTimelock,
+}
+
+type Proposal interface {
+	ToExecutor(sim bool) (*mcms.Executor, error)
+	AddSignature(signature mcms.Signature)
+	Validate() error
+}
+
+func LoadProposal(proposalType ProposalType, filePath string) (Proposal, error) {
+	switch proposalType {
+	case MCMS:
+		return mcms.NewProposalFromFile(filePath)
+	case MCMSWithTimelock:
+		return timelock.NewMCMSWithTimelockProposalFromFile(filePath)
+	default:
+		return nil, errors.New("unknown proposal type")
+	}
+}
