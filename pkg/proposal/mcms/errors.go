0f1b0b4c388c10438c5a163127b4a1f63bbce9d1
Pablo <pablo.estrada@smartcontract.com>
2024-10-28 09:18:26 -0600

    feat: add helper function for error

--- pkg/proposal/mcms/errors.go
+++ pkg/proposal/mcms/errors.go
@@ -6,7 +6,7 @@ import (
 	chain_selectors "github.com/smartcontractkit/chain-selectors"
 )
 
-type UnkownChainSelectorFamilyError struct {
+type UnknownChainSelectorFamilyError struct {
 	ChainSelector uint64
 	ChainFamily   string
 }
@@ -16,6 +16,13 @@ var SupportedChainSelectorFamilies = []string{
 	chain_selectors.FamilySolana,
 }
 
-func (e UnkownChainSelectorFamilyError) Error() string {
+func (e UnknownChainSelectorFamilyError) Error() string {
 	return fmt.Sprintf("unknown chain selector family: %d with family %s. Supported families are %v", e.ChainSelector, e.ChainFamily, SupportedChainSelectorFamilies)
+}
+
+func NewUnknownChainSelectorFamilyError(selector uint64, family string) *UnknownChainSelectorFamilyError {
+	return &UnknownChainSelectorFamilyError{
+		ChainSelector: selector,
+		ChainFamily:   family,
+	}
 }
