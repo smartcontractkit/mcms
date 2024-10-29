0791f58fa46c7aa903b2240b589283883ccdda35
Akhil Chainani <akhil.chainani@smartcontract.com>
2024-10-29 16:12:52 -0700

    add signable tests

--- internal/proposal/mcms/executable_test.go
+++ internal/proposal/mcms/executable_test.go
@@ -586,6 +586,7 @@ func TestExecutor_ExecuteE2E_SingleChainMultipleSignerMultipleTX_Success(t *test
 	// Construct a proposal
 	proposal := MCMSProposal{
 		Version:              "1.0",
+		Description:          "Grants RBACTimelock 'Proposer','Canceller','Executor', and 'Bypasser' Role to MCMS Contract",
 		ValidUntil:           2004259681,
 		Signatures:           []mcms.Signature{},
 		OverridePreviousRoot: false,
@@ -606,19 +607,10 @@ func TestExecutor_ExecuteE2E_SingleChainMultipleSignerMultipleTX_Success(t *test
 	require.NoError(t, err)
 	assert.NotNil(t, signable)
 
-	// Get the hash to sign
-	hash, err := signable.SigningHash()
-	require.NoError(t, err)
-
 	// Sign the hash
 	for i := range 3 {
-		sig, serr := crypto.Sign(hash.Bytes(), keys[i])
-		require.NoError(t, serr)
-
-		// Construct a signature
-		sigObj, serr := mcms.NewSignatureFromBytes(sig)
-		require.NoError(t, serr)
-		proposal.Signatures = append(proposal.Signatures, sigObj)
+		err = proposal_core.SignPlainKey(keys[i], &proposal, true, inspectors)
+		require.NoError(t, err)
 	}
 
 	// Validate the signatures
@@ -681,199 +673,4 @@ func TestExecutor_ExecuteE2E_SingleChainMultipleSignerMultipleTX_Success(t *test
 		require.NoError(t, err)
 		assert.Equal(t, mcmsObj.Address().Hex(), roleMember.Hex())
 	}
-}
-
-func TestExecutor_ExecuteE2E_SingleChainMultipleSignerMultipleTX_FailureMissingQuorum(t *testing.T) {
-	t.Parallel()
-
-	keys, auths, sim, mcmsObj, err := setupSimulatedBackendWithMCMS(3)
-	require.NoError(t, err)
-	assert.NotNil(t, sim)
-	assert.NotNil(t, mcmsObj)
-	for i := range 3 {
-		assert.NotNil(t, keys[i])
-		assert.NotNil(t, auths[i])
-	}
-
-	// Deploy a timelock contract for testing
-	addr, tx, timelock, err := bindings.DeployRBACTimelock(
-		auths[0],
-		sim,
-		big.NewInt(0),
-		mcmsObj.Address(),
-		[]common.Address{},
-		[]common.Address{},
-		[]common.Address{},
-		[]common.Address{},
-	)
-	require.NoError(t, err)
-	assert.NotNil(t, addr)
-	assert.NotNil(t, tx)
-	assert.NotNil(t, timelock)
-	sim.Commit()
-
-	// Construct example transactions
-	proposerRole, err := timelock.PROPOSERROLE(&bind.CallOpts{})
-	require.NoError(t, err)
-	bypasserRole, err := timelock.BYPASSERROLE(&bind.CallOpts{})
-	require.NoError(t, err)
-	cancellerRole, err := timelock.CANCELLERROLE(&bind.CallOpts{})
-	require.NoError(t, err)
-	executorRole, err := timelock.EXECUTORROLE(&bind.CallOpts{})
-	require.NoError(t, err)
-	timelockAbi, err := bindings.RBACTimelockMetaData.GetAbi()
-	require.NoError(t, err)
-
-	operations := make([]mcms.ChainOperation, 4)
-	for i, role := range []common.Hash{proposerRole, bypasserRole, cancellerRole, executorRole} {
-		data, perr := timelockAbi.Pack("grantRole", role, mcmsObj.Address())
-		require.NoError(t, perr)
-		operations[i] = mcms.ChainOperation{
-			ChainSelector: TestChain1,
-			Operation: evm.NewEVMOperation(
-				timelock.Address().Hex(),
-				data,
-				big.NewInt(0),
-				"Sample contract",
-				[]string{"tag1", "tag2"},
-			),
-		}
-	}
-
-	// Construct a proposal
-	proposal := MCMSProposal{
-		Version:              "1.0",
-		Description:          "Grants RBACTimelock 'Proposer','Canceller','Executor', and 'Bypasser' Role to MCMS Contract",
-		ValidUntil:           2004259681,
-		Signatures:           []mcms.Signature{},
-		OverridePreviousRoot: false,
-		ChainMetadata: map[mcms.ChainSelector]mcms.ChainMetadata{
-			TestChain1: {
-				StartingOpCount: 0,
-				MCMAddress:      mcmsObj.Address().Hex(),
-			},
-		},
-		Transactions: operations,
-	}
-
-	// Gen caller map for easy access
-	inspectors := map[mcms.ChainSelector]mcms.Inspector{TestChain1: evm.NewEVMInspector(sim)}
-
-	// Construct executor
-	signable, err := proposal.Signable(true, inspectors)
-	require.NoError(t, err)
-	assert.NotNil(t, signable)
-
-	// Sign the hash
-	for i := range 2 {
-		err = proposal_core.SignPlainKey(keys[i], &proposal, true, inspectors)
-		require.NoError(t, err)
-	}
-
-	// Validate the signatures
-	quorumMet, err := signable.ValidateSignatures()
-	assert.False(t, quorumMet)
-	require.Error(t, err)
-	// assert error is of type QuorumNotMetError
-	assert.IsType(t, &mcms_core.QuorumNotMetError{}, err)
-}
-
-func TestExecutor_ExecuteE2E_SingleChainMultipleSignerMultipleTX_FailureInvalidSigner(t *testing.T) {
-	t.Parallel()
-
-	keys, auths, sim, mcmsObj, err := setupSimulatedBackendWithMCMS(3)
-	require.NoError(t, err)
-	assert.NotNil(t, sim)
-	assert.NotNil(t, mcmsObj)
-	for i := range 3 {
-		assert.NotNil(t, keys[i])
-		assert.NotNil(t, auths[i])
-	}
-
-	// Generate a new key
-	newKey, err := crypto.GenerateKey()
-	require.NoError(t, err)
-	keys[2] = newKey
-
-	// Deploy a timelock contract for testing
-	addr, tx, timelock, err := bindings.DeployRBACTimelock(
-		auths[0],
-		sim,
-		big.NewInt(0),
-		mcmsObj.Address(),
-		[]common.Address{},
-		[]common.Address{},
-		[]common.Address{},
-		[]common.Address{},
-	)
-	require.NoError(t, err)
-	assert.NotNil(t, addr)
-	assert.NotNil(t, tx)
-	assert.NotNil(t, timelock)
-	sim.Commit()
-
-	// Construct example transactions
-	proposerRole, err := timelock.PROPOSERROLE(&bind.CallOpts{})
-	require.NoError(t, err)
-	bypasserRole, err := timelock.BYPASSERROLE(&bind.CallOpts{})
-	require.NoError(t, err)
-	cancellerRole, err := timelock.CANCELLERROLE(&bind.CallOpts{})
-	require.NoError(t, err)
-	executorRole, err := timelock.EXECUTORROLE(&bind.CallOpts{})
-	require.NoError(t, err)
-	timelockAbi, err := bindings.RBACTimelockMetaData.GetAbi()
-	require.NoError(t, err)
-
-	operations := make([]mcms.ChainOperation, 4)
-	for i, role := range []common.Hash{proposerRole, bypasserRole, cancellerRole, executorRole} {
-		data, perr := timelockAbi.Pack("grantRole", role, mcmsObj.Address())
-		require.NoError(t, perr)
-		operations[i] = mcms.ChainOperation{
-			ChainSelector: TestChain1,
-			Operation: evm.NewEVMOperation(
-				timelock.Address().Hex(),
-				data,
-				big.NewInt(0),
-				"Sample contract",
-				[]string{"tag1", "tag2"},
-			),
-		}
-	}
-
-	// Construct a proposal
-	proposal := MCMSProposal{
-		Version:              "1.0",
-		Description:          "Grants RBACTimelock 'Proposer','Canceller','Executor', and 'Bypasser' Role to MCMS Contract",
-		ValidUntil:           2004259681,
-		Signatures:           []mcms.Signature{},
-		OverridePreviousRoot: false,
-		ChainMetadata: map[mcms.ChainSelector]mcms.ChainMetadata{
-			TestChain1: {
-				StartingOpCount: 0,
-				MCMAddress:      mcmsObj.Address().Hex(),
-			},
-		},
-		Transactions: operations,
-	}
-
-	// Gen caller map for easy access
-	inspectors := map[mcms.ChainSelector]mcms.Inspector{TestChain1: evm.NewEVMInspector(sim)}
-
-	// Construct executor
-	signable, err := proposal.Signable(true, inspectors)
-	require.NoError(t, err)
-	assert.NotNil(t, signable)
-
-	// Sign the hash
-	for i := range 3 {
-		err = proposal_core.SignPlainKey(keys[i], &proposal, true, inspectors)
-		require.NoError(t, err)
-	}
-
-	// Validate the signatures
-	quorumMet, err := signable.ValidateSignatures()
-	assert.False(t, quorumMet)
-	require.Error(t, err)
-	// assert error is of type QuorumNotMetError
-	assert.IsType(t, &mcms_core.InvalidSignatureError{}, err)
 }
