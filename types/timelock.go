6ee9fd7c349cb56abf5c405b70700da7a8368a38
James Kong <jkongie@gmail.com>
2024-11-01 01:49:03 +0800

    chore: moves BatchChainOperation to the types package
    Moves the BatchChainOperation struct to the types package and updates
    references, adding additional documentation.

--- types/timelock.go
+++ types/timelock.go
@@ -1,9 +1,22 @@
 package types
 
+// TimelockAction is an enum for the different types of timelock actions that can be performed on
+// a timelock proposal.
 type TimelockAction string
 
 const (
+	// TimelockActionExecute sets up transactions to execute after a delay.
 	TimelockActionSchedule TimelockAction = "schedule"
-	TimelockActionCancel   TimelockAction = "cancel"
-	TimelockActionBypass   TimelockAction = "bypass"
+
+	// TimelockActionCancel cancels previously scheduled transactions.
+	TimelockActionCancel TimelockAction = "cancel"
+
+	// TimelockActionBypass directly executes transactions, skipping the timelock.
+	TimelockActionBypass TimelockAction = "bypass"
 )
+
+// BatchChainOperation is a struct that represents a batch of operations to be executed on a chain.
+type BatchChainOperation struct {
+	ChainSelector ChainSelector `json:"chainIdentifier"`
+	Batch         []Operation   `json:"batch"`
+}
