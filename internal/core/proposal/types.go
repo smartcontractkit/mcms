1c2815951983895e02f0d398d5bdc326257bb047
Pablo Estrada <139084212+ecPablo@users.noreply.github.com>
2024-11-07 09:54:07 -0600

    chore: move ProposalType to the types folder [DPA-1217] (#118)

--- internal/core/proposal/types.go
+++ internal/core/proposal/types.go
@@ -4,20 +4,6 @@ import (
 	"github.com/smartcontractkit/mcms/types"
 )
 
-type ProposalType string
-
-const (
-	// MCMSProposalType is a proposal type for the MCMS contract.
-	MCMS ProposalType = "MCMS"
-	// MCMSWithTimelock is a proposal type for the MCMS contract with timelock.
-	MCMSWithTimelock ProposalType = "MCMSWithTimelock"
-)
-
-var StringToProposalType = map[string]ProposalType{
-	"MCMS":             MCMS,
-	"MCMSWithTimelock": MCMSWithTimelock,
-}
-
 type Executable interface {
 	SetRoot(chainSelector types.ChainSelector) (string, error)
 	Execute(index int) (string, error)
