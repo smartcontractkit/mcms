b67a29877c6b1355030ca8a492702b1f3e6fecf0
Akhil Chainani <akhil.chainani@smartcontract.com>
2024-12-03 04:18:57 -0500

    add LoadProposal tests

---
+++ utils_test.go
@@ -0,0 +1,31 @@
+package mcms
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadProposal(t *testing.T) {
+	t.Run("should return error for invalid JSON", func(t *testing.T) {
+		input := `{invalid json}`
+		reader := strings.NewReader(input)
+
+		proposal, err := LoadProposal(reader)
+
+		assert.Error(t, err)
+		assert.Nil(t, proposal)
+	})
+
+	t.Run("should return error for unknown proposal type", func(t *testing.T) {
+		input := `{"kind": "unknown_type"}`
+		reader := strings.NewReader(input)
+
+		proposal, err := LoadProposal(reader)
+
+		assert.Error(t, err)
+		assert.Nil(t, proposal)
+		assert.Equal(t, "unknown proposal type", err.Error())
+	})
+}
