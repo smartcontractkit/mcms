012d2471b62c7427a596049f025d863971a1ab71
Pablo <pablo.estrada@smartcontract.com>
2025-01-09 14:43:13 -0600

    fix: merge conflicts main

--- sdk/solana/executor.go
+++ sdk/solana/executor.go
@@ -7,8 +7,8 @@ import (
 	"math"
 
 	"github.com/ethereum/go-ethereum/common"
+
 	"github.com/gagliardetto/solana-go"
-	solana "github.com/gagliardetto/solana-go"
 	"github.com/gagliardetto/solana-go/rpc"
 
 	"github.com/smartcontractkit/chainlink-ccip/chains/solana/contracts/tests/config"
