ebb39c72dac4149a4480cbda899c1327a275824d
Friedemann Fürst <59653747+friedemannf@users.noreply.github.com>
2025-04-15 17:15:21 +0200

    Remove test helper

--- e2e/tests/aptos/common.go
+++ e2e/tests/aptos/common.go
@@ -3,12 +3,10 @@
 package aptos
 
 import (
-	"os"
 	"time"
 
 	"github.com/aptos-labs/aptos-go-sdk"
 	"github.com/aptos-labs/aptos-go-sdk/crypto"
-	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/suite"
 
 	cselectors "github.com/smartcontractkit/chain-selectors"
@@ -32,35 +30,20 @@ type AptosTestSuite struct {
 }
 
 func (a *AptosTestSuite) SetupSuite() {
-	testnet := false
-	if testnet {
-		// TODO remove this after testing
-		a.TestSetup.AptosRPCClient, _ = aptos.NewNodeClient("https://api.testnet.aptoslabs.com/v1", 2)
-		a.ChainSelector = types.ChainSelector(cselectors.APTOS_TESTNET.Selector)
-		deployerKey := &crypto.Ed25519PrivateKey{}
-		err := godotenv.Load("../custom_configs/.env")
-		a.Require().NoError(err)
-		userKey := os.Getenv("USER_KEY")
-		err = deployerKey.FromHex(userKey)
-		a.Require().NoError(err)
-		a.deployerAccount, err = aptos.NewAccountFromSigner(deployerKey)
-		a.Require().NoError(err)
-	} else {
-		a.TestSetup = *e2e.InitializeSharedTestSetup(a.T())
-		details, err := cselectors.GetChainDetailsByChainIDAndFamily(a.AptosChain.ChainID, cselectors.FamilyAptos)
-		a.Require().NoError(err)
-		a.ChainSelector = types.ChainSelector(details.ChainSelector)
+	a.TestSetup = *e2e.InitializeSharedTestSetup(a.T())
+	details, err := cselectors.GetChainDetailsByChainIDAndFamily(a.AptosChain.ChainID, cselectors.FamilyAptos)
+	a.Require().NoError(err)
+	a.ChainSelector = types.ChainSelector(details.ChainSelector)
 
-		// Set up deployer account, it's automatically funded by CTF when setting up the Aptos chain
-		// Instead of using (*Ed25519PrivateKey).FromHex directly, parse manually to pass the strict=false flag
-		bytes, err := crypto.ParsePrivateKey(blockchain.DefaultAptosPrivateKey, crypto.PrivateKeyVariantEd25519, false)
-		a.Require().NoError(err)
-		deployerKey := &crypto.Ed25519PrivateKey{}
-		err = deployerKey.FromBytes(bytes)
-		a.Require().NoError(err)
-		a.deployerAccount, err = aptos.NewAccountFromSigner(deployerKey)
-		a.Require().NoError(err)
-	}
+	// Set up deployer account, it's automatically funded by CTF when setting up the Aptos chain
+	// Instead of using (*Ed25519PrivateKey).FromHex directly, parse manually to pass the strict=false flag
+	bytes, err := crypto.ParsePrivateKey(blockchain.DefaultAptosPrivateKey, crypto.PrivateKeyVariantEd25519, false)
+	a.Require().NoError(err)
+	deployerKey := &crypto.Ed25519PrivateKey{}
+	err = deployerKey.FromBytes(bytes)
+	a.Require().NoError(err)
+	a.deployerAccount, err = aptos.NewAccountFromSigner(deployerKey)
+	a.Require().NoError(err)
 }
 
 func (a *AptosTestSuite) deployMCMSContract() {
