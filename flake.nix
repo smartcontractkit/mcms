a8fe126bd95606b46ed9e88d39fc439ce3e5e241
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2025-07-14 15:18:21 +0200

    add basic nix shell

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "MCMS SDK";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in rec {
+      devShell = pkgs.callPackage ./shell.nix {};
+    });
+}
---
+++ shell.nix
@@ -0,0 +1,43 @@
+{
+  stdenv,
+  pkgs,
+  lib,
+}:
+(pkgs.pkgs.mkShell {
+  buildInputs = with pkgs;
+    [
+      # Development
+      go-task
+      golangci-lint
+
+      # Required for Sui CLI (Move compilation)
+      git
+      # Go 1.24 + tools
+      go_1_24
+      gopls
+      mockgen
+
+      # Keep adding as needed
+
+      # Sui CLI custom derivation
+      (pkgs.callPackage ./sui.nix {})
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      libiconv
+    ];
+
+  shellHook = ''
+    echo "Setting up clean Go environment (disabling GVM)..."
+    # Unset GVM environment leakage
+    unset GOROOT
+    unset GOPATH
+    unset GOTOOLDIR
+    # Add Nix-provided Go binary path to ensure consistency
+    export PATH=$(go env GOROOT)/bin:$PATH
+    # Optional: lock Go toolchain version if needed
+    export GOTOOLCHAIN=go1.24.2+auto
+    # Debug info
+    echo "Using Go at: $(which go)"
+    go version
+  '';
+})
