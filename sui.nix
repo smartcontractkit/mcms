3beeb00b7cd1d680d6899fee2c5df9f8c549c7e4
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2025-07-14 15:18:21 +0200

    add basic nix shell

---
+++ sui.nix
@@ -0,0 +1,32 @@
+{
+  stdenv,
+  pkgs,
+}:
+stdenv.mkDerivation rec {
+  name = "sui-cli-${version}";
+  version = "1.50.1"; # Update as needed. Should be a mainnet release version from https://github.com/MystenLabs/sui/releases
+
+  src = if stdenv.hostPlatform.isDarwin then
+    pkgs.fetchzip {
+      url = "https://github.com/MystenLabs/sui/releases/download/mainnet-v${version}/sui-mainnet-v${version}-macos-arm64.tgz"; # Assume is a M1 Mac
+      sha256 = "sha256-LWi6RVLg7fJ5aHBQ/02zxZ6SokR59TOsfHXupStNZZs=";  # Should be replaced when bumping versions
+      stripRoot = false;
+    }
+    else if stdenv.isLinux then
+      pkgs.fetchzip {
+        url = "https://github.com/MystenLabs/sui/releases/download/mainnet-v${version}/sui-mainnet-v${version}-ubuntu-x86_64.tgz";
+        sha256 = "sha256-nyG4veT0qWIJI+j4XGk6+la2Gy+G5TJ+lw9kvi5b8s4=";  # Should be replaced when bumping versions
+        stripRoot = false;
+      }
+    else
+      builtins.throw "Unsupported system";
+
+  sourceRoot = ".";
+
+  # No build needed since we're using a prebuilt binary archive.
+  buildPhase = "true";
+  installPhase = ''
+    mkdir -p $out/bin
+    mv source/sui $out/bin/
+  '';
+}
