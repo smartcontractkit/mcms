c1764243c59d4acdea8a69a09d3867597d72593f
Kristijan <kristijan.rebernisak@gmail.com>
2025-12-02 18:17:40 +0100

    Fix lint errors #14 (wip)

--- e2e/tests/solana/simulator.go
+++ e2e/tests/solana/simulator.go
@@ -23,7 +23,7 @@ import (
 
 var testPDASeedSetRootSimulateTest = [32]byte{'t', 'e', 's', 't', '-', 's', 'e', 't', 'r', 'o', 'o', 't', '-', 's', 'i', 'm', 'u', 'l', 'a', 't', 'e'}
 
-func (s *TestSuite) TestSimulatorSimulateSetRoot() {
+func (s *TestSuite) TestSimulator_SimulateSetRoot() {
 	s.SetupMCM(testPDASeedSetRootSimulateTest)
 	ctx := context.Background()
 
@@ -120,7 +120,7 @@ func (s *TestSuite) TestSimulatorSimulateSetRoot() {
 	s.Require().NoError(err)
 }
 
-func (s *TestSuite) TestSimulatorSimulateOperation() {
+func (s *TestSuite) TestSimulator_SimulateOperation() {
 	ctx := context.Background()
 
 	recipientAddress, err := solana.NewRandomPrivateKey()
