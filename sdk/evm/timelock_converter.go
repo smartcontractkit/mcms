0152e6244da3119717b7cbe979dff046d6270a47
Pablo Estrada <139084212+ecPablo@users.noreply.github.com>
2026-01-14 12:36:03 -0600

    fix: change pointer receiver evm converter (#576)
    Changing for consistency across the rest of chain family sdks
    
    ---------
    
    Signed-off-by: Pablo <pablo.estrada@smartcontract.com>

--- sdk/evm/timelock_converter.go
+++ sdk/evm/timelock_converter.go
@@ -26,7 +26,7 @@ func NewTimelockConverter() *TimelockConverter {
 	return &TimelockConverter{}
 }
 
-func (t *TimelockConverter) ConvertBatchToChainOperations(
+func (t TimelockConverter) ConvertBatchToChainOperations(
 	_ context.Context,
 	metadata types.ChainMetadata,
 	bop types.BatchOperation,
