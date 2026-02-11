ff71d17d173fb700e4939c35371776ed5cf53421
Pablo <pablo.estrada@smartcontract.com>
2026-02-10 23:36:15 -0600

    fix: tests
    Signed-off-by: Pablo <pablo.estrada@smartcontract.com>

--- types/chain_selector.go
+++ types/chain_selector.go
@@ -41,6 +41,12 @@ func GetChainSelectorFamily(sel ChainSelector) (string, error) {
 	ctx := context.Background()
 	details, err := chainselremote.GetChainDetailsBySelector(ctx, uint64(sel), chainselremote.WithFallbackToLocal(true))
 	if err != nil {
+		// Remote selectors may not include dev/test selectors; fall back to local map.
+		family, localErr := chainsel.GetSelectorFamily(uint64(sel))
+		if localErr == nil {
+			return family, nil
+		}
+
 		return "", fmt.Errorf("%w for selector %d", ErrChainFamilyNotFound, sel)
 	}
 
